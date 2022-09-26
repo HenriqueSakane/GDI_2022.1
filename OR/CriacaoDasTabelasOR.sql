38af3f86350139480fa4b3acdf9595f88b3e48c6
IgorDomingos <idrs@cin.ufpe.br>
2022-09-26 09:44:01 -0300

    Iniciando Parte OR

---
+++ OR/CriacaoDasTabelasOR.sql
@@ -0,0 +1,56 @@
+-- Criação das tabelas
+/* CHECKLIST: 
+
+-- CREATE OR REPLACE TYPE 
+-- CREATE OR REPLACE TYPE BODY
+-- MEMBER PROCEDURE
+-- MEMBER FUNCTION
+-- ORDER MEMBER FUNCTION
+-- MAP MEMBER FUNCTION
+-- CONSTRUCTOR FUNCTION
+-- OVERRIDING MEMBER
+-- FINAL MEMBER
+-- NOT INSTANTIABLE TYPE/MEMBER
+-- HERANÇA DE TIPOS (UNDER/NOT FINAL)
+-- ALTER TYPE
+-- CREATE TABLE OF
+-- WITH ROWID REFERENCES
+-- REF
+-- SCOPE IS
+-- INSERT INTO
+-- VALUE
+-- VARRAY
+-- NESTED TABLE
+*/ 
+
+/*
+Tabelas:
+
+Pessoa
+Telefone 
+Endereço
+Cargo
+Funcionário
+Dependente
+Biólogo 
+Atendente
+Zelador 
+Visitante 
+Jaula 
+Departamento 
+Animais
+Cuida 
+Limpa 
+Ticket 
+Compra 
+Promoção 
+Participa
+Pertence 
+*/
+CREATE TABLE tb_pessoa OF tp_pessoa (
+	cpf PRIMARY KEY,
+	nome NOT NULL,
+	data_nascimento NOT NULL
+);
+
+CREATE TABLE tb_funcionario OF tp_funcionario ()
---
+++ OR/CriacaoDosTipos.sql
@@ -0,0 +1,146 @@
+-- Criação dos tipos
+
+-- Pessoa 
+CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
+	cpf VARCHAR2(15),
+	nome VARCHAR(25),
+	data_nascimento DATE
+);
+
+-- Endereço
+CREATE OR REPLACE TYPE tp_endereco AS OBJECT (
+	cpf_pessoa REF tp_pessoa,
+	cep VARCHAR2(9),
+	rua VARCHAR2(30),
+	numero NUMBER,
+	bairro VARCHAR2(20)
+);
+
+-- Telefone 
+CREATE OR REPLACE TYPE tp_telefone AS OBJECT (
+	cpf_pessoa REF tp_pessoa,
+	numero_telefone NUMBER
+);
+
+-- cargo
+CREATE OR REPLACE TYPE tp_cargo AS OBJECT (
+	cargo_funcionario VARCHAR2 (20),
+	salario NUMBER 
+);
+
+-- Funcionario (herda de pessoa)
+CREATE OR REPLACE TYPE tp_funcionario UNDER tp_pessoa (
+	cargo REF tp_cargo,
+	cpf_supervisor REF tp_funcionario
+);
+
+-- Dependente 
+CREATE OR REPLACE TYPE tp_dependente AS OBJECT (
+	cpf_funcionario REF tp_funcionario,
+	nome_dependente VARCHAR2(20),
+	grau_parentesco VARCHAR(10),
+	data_nasc DATE
+);
+
+-- Biologo
+-- herda de funcionario 
+CREATE OR REPLACE TYPE tp_biologo UNDER tp_funcionario (
+	especializacao VARCHAR(15)
+);
+
+-- Atendente
+-- herda de funcionario
+CREATE OR REPLACE TYPE tp_atendente UNDER tp_funcionario (
+	guiche NUMBER 
+); 
+
+-- Zelador
+-- herda de funcionario 
+CREATE OR REPLACE TYPE tp_zelador UNDER tp_funcionario (
+	numero_de_jaulas_a_limpar NUMBER 
+);
+
+-- Visitante 
+CREATE OR REPLACE TYPE tp_visitante UNDER tp_pessoa (
+	data_cadastro DATE,]
+	carteira_de_estudante VARCHAR(12)
+);
+
+-- Jaula 
+CREATE OR REPLACE TYPE tp_jaula AS OBJECT (
+	cod_jaula VARCHAR2(5),
+	capacidade NUMBER
+);
+
+-- Departamento
+CREATE OR REPLACE TYPE tp_departamento AS OBJECT (
+	cod_departamento VARCHAR2(5),
+	nome_departamento VARCHAR2,
+	quantidade_de_jaulas NUMBER
+);
+
+-- Animais 
+CREATE OR REPLACE TYPE tp_animais AS OBJECT (
+	cod_animal VARCHAR2(5),
+	especie VARCHAR2(20)
+);
+
+-- Cuida
+CREATE OR REPLACE TYPE tp_cuida AS OBJECT (
+	cod_animal REF tp_animais,
+	cpf_biologo REF tp_biologo
+);
+
+-- Limpa 
+CREATE OR REPLACE TYPE tp_limpa AS OBJECT (
+	cpf_zelador REF tp_zelador,
+	cod_jaula REF tp_jaula
+);
+
+-- Ticket 
+CREATE OR REPLACE TYPE tp_ticket AS OBJECT (
+	numero_ticket NUMBER,
+	cpf_atendente REF tp_atendente,
+	valor NUMBER,
+	data_ticket DATE
+);
+
+-- Compra 
+CREATE OR REPLACE TYPE tp_compra AS OBJECT (
+	numero_ticket NUMBER,
+	cpf_visitante VARCHAR2(15),
+	nota_fiscal INTEGER
+);
+
+-- Promocao 
+CREATE OR REPLACE TYPE tp_promocao AS OBJECT (
+	cod_promocao NUMBER,
+	desconto NUMBER,
+	data_inicio DATE,
+	data_termino DATE
+);
+
+-- Participa 
+CREATE OR REPLACE TYPE tp_participa AS OBJECT (
+	cod_promocao REF tp_promocao,
+	numero_ticket REF tp_ticket,
+	cpf_visitante REF tp_visitante
+);
+
+-- Pertence 
+CREATE OR REPLACE TYPE tp_pertence AS OBJECT (
+	animais REF tp_animais,
+	jaula REF tp_jaula,
+	data_entrada DATE, 
+	departamento REF tp_departamento,
+	data_saida DATE
+);
+
+
+
+
+
+
+
+
+
