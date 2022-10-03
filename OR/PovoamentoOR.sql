dbfd74ac9ab7314bac3ecb58f29fc2fde64dc79a
DanielVCC <43818658+DanielVCC@users.noreply.github.com>
2022-10-03 19:44:54 -0300

    povoamento  telefone

--- OR/PovoamentoOR.sql
+++ OR/PovoamentoOR.sql
@@ -1,2 +1,33 @@
 -- Povoamento OR
 
+-- Telefone
+
+INSERT INTO tb_telefone VALUES (tp_telefone('111.222.333-44', '987776666'));
+INSERT INTO tb_telefone VALUES (tp_telefone('444.555.666-77', '978886666'));
+INSERT INTO tb_telefone VALUES (tp_telefone('444.555.666-77', '32221111'));
+INSERT INTO tb_telefone VALUES (tp_telefone('777.888.999-99', '998887777'));
+INSERT INTO tb_telefone VALUES (tp_telefone('123.456.789-00', '994445555'));
+INSERT INTO tb_telefone VALUES (tp_telefone('123.456.789-00', '34440000'));
+INSERT INTO tb_telefone VALUES (tp_telefone('248.369.124-22', '989995555'));
+INSERT INTO tb_telefone VALUES (tp_telefone('111.111.111-11', '987773333'));
+INSERT INTO tb_telefone VALUES (tp_telefone('222.222.222-22', '997774444'));
+INSERT INTO tb_telefone VALUES (tp_telefone('333.333.333-33', '996662222'));
+INSERT INTO tb_telefone VALUES (tp_telefone('333.333.333-33', '987550101'));
+INSERT INTO tb_telefone VALUES (tp_telefone('333.333.333-33', '34110000'));
+INSERT INTO tb_telefone VALUES (tp_telefone('444.444.444-44', '993338888'));
+INSERT INTO tb_telefone VALUES (tp_telefone('555.555.555-55', '981110000'));
+INSERT INTO tb_telefone VALUES (tp_telefone('666.666.666-66', '988885555'));
+INSERT INTO tb_telefone VALUES (tp_telefone('666.666.666-66', '34444444'));
+INSERT INTO tb_telefone VALUES (tp_telefone('777.777.777-77', '999990000'));
+INSERT INTO tb_telefone VALUES (tp_telefone('888.888.888-88', '991911919'));
+INSERT INTO tb_telefone VALUES (tp_telefone('999.999.999-99', '991919191'));
+INSERT INTO tb_telefone VALUES (tp_telefone('999.999.999-99', '32322323'));
+INSERT INTO tb_telefone VALUES (tp_telefone('101.101.101-10', '999009900'));
+INSERT INTO tb_telefone VALUES (tp_telefone('202.202.202-20', '997777777'));
+INSERT INTO tb_telefone VALUES (tp_telefone('202.202.202-20', '32323232'));
+INSERT INTO tb_telefone VALUES (tp_telefone('202.202.202-20', '998988989'));
+INSERT INTO tb_telefone VALUES (tp_telefone('303.303.303-30', '998989898'));
+INSERT INTO tb_telefone VALUES (tp_telefone('404.404.404-40', '997977979'));
+INSERT INTO tb_telefone VALUES (tp_telefone('505.505.505-55', '997979797'));
+INSERT INTO tb_telefone VALUES (tp_telefone('606.606.606-60', '993336666'));
+INSERT INTO tb_telefone VALUES (tp_telefone('606.606.606-60', '32326464'));
