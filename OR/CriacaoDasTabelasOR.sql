e2313670a9e16aa4b95b402a8a2d92320ae1591e
Igor Domingos <48846754+IgorDomingos@users.noreply.github.com>
2022-10-04 21:29:05 -0300

    Adicionando as tabelas que faltavam

--- OR/CriacaoDasTabelasOR.sql
+++ OR/CriacaoDasTabelasOR.sql
@@ -58,34 +58,115 @@ CREATE TABLE tb_endereco OF tp_endereco (
 	cpf_pessoa SCOPE IS tb_pessoa
 );
 
+-- Cargo 
+CREATE TABLE tb_cargo OF tp_cargo (
+	cargo_funcionario PRIMARY KEY,
+	salario NOT NULL
+);
+
+-- telefone
 CREATE TABLE tb_telefone OF tp_telefone (
 	cpf_pessoa WITH ROWID REFERENCES tb_pessoa PRIMARY KEY, -- checar
 	numero_telefone PRIMARY KEY
 );
 
+-- funcionario
 CREATE TABLE tb_funcionario OF tp_funcionario (
 	cargo SCOPE IS tb_cargo,
 	cpf_supervisor SCOPE IS tb_funcionario
 );
 
-CREATE TABLE tb_biologo OF tp_biologo  (
-	cpf_biologo SCOPE IS tb_funcionario,
+-- DEPENDENTE
+
+-- Biólogo 
+CREATE TABLE tb_biologo OF tp_biologo (
+	cpf PRIMARY KEY,
 	especializacao NOT NULL
 );
 
+-- Atendente
+CREATE TABLE tb_atendente OF tp_atendente (
+	cpf PRIMARY KEY,
+	guiche NOT NULL
+);
+
+-- Zelador
+CREATE TABLE tb_zelador OF tp_zelador (
+	cpf PRIMARY KEY,
+	numero_de_jaulas_a_limpar NOT NULL
+);
+
+-- Visitante
+CREATE TABLE tb_visitante OF tp_visitante (
+	cpf PRIMARY KEY,
+	data_cadastro NOT NULL
+);
+
+-- Jaula 
+CREATE TABLE tb_jaula OF tp_jaula (
+	cod_jaula PRIMARY KEY,
+	capacidade NOT NULL
+);
+-- departamento
 CREATE TABLE tb_departamento OF tp_departamento (
 	cod_departamento PRIMARY KEY,
 	nome_departamento NOT NULL,
 	quantidade_de_jaulas NOT NULL
 );
 
+-- Animais
+CREATE TABLE tb_animais OF tp_aninmais (
+	cod_animal PRIMARY KEY,
+	especie NOT NULL
+);
+
+-- Cuida
+CREATE TABLE tb_cuida OF tp_cuida (
+	cod_animal WITH ROWID REFERENCES tb_animais,
+	cpf_biologo WITH ROWID REFERENCES tb_biologo
+);
+
+-- Limpa
+CREATE TABLE tb_limpa OF tp_limpa (
+	cod_jaula WITH ROWID REFERENCES tb_jaula,
+	cpf_zelador WITH ROWID REFERENCES tb_zelador
+);
+
+-- Ticket
+CREATE TABLE tb_ticket OF tp_ticket (
+	numero_ticket PRIMARY KEY,
+	cpf_atendente WITH ROWID REFERENCES tb_atendente,
+	valor NOT NULL,
+	data_ticket NOT NULL
+);
+
+-- compra
 CREATE TABLE tb_compra OF tp_compra (
 	numero_ticket PRIMARY KEY,
 	cpf_visitante PRIMARY KEY,
 	nota_fiscal NOT NULL,
 );
 
-CREATE TABLE tb_zelador OF tp_zelador  (
-	cpf_zelador SCOPE IS tb_funcionario,
-	numero_de_jaulas_a_limpar NOT NULL
+-- Promoção 
+CREATE TABLE tb_promocao OF tp_promocao (
+	cod_promocao PRIMARY KEY,
+	desconto NOT NULL,
+	data_inicio NOT NULL,
+	data_termino NOT NULL
+);
+
+-- Participa
+CREATE TABLE tb_participa OF tp_participa (
+	cod_promocao WITH ROWID REFERENCES tp_promocao,
+	numero_ticket WITH ROWID REFERENCES tp_ticket,
+	cpf_visitante WITH ROWID REFERENCES tp_visitante
+);
+
+-- Pertence 
+CREATE TABLE tb_pertence OF tp_pertence (
+	animais WITH ROWID REFEENCES tp_animais,
+	jaula WITH ROWID REFERENCES tp_jaulas,
+	data_entrada PRIMARY KEY,
+	departamento WITH ROWID REFERENCES tp_departamento,
+	data_saida NOT NULL
 );
