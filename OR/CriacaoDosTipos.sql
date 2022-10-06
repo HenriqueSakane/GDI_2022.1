c5b9b61d98ae71e9994a74f9b461251418ad3653
HOBS <43381709+HenriqueSakane@users.noreply.github.com>
2022-10-05 23:20:39 -0300

    Update CriacaoDosTipos.sql
    Correçao do overriding em tp_funcionario

--- OR/CriacaoDosTipos.sql
+++ OR/CriacaoDosTipos.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE TYPE tp_endereco AS OBJECT (
 	numero NUMBER,
 	bairro VARCHAR2(20)
 );
-
+/
 CREATE OR REPLACE TYPE BODY tp_endereco AS
 	CONSTRUCTOR FUNCTION tp_endereco(SELF IN OUT NOCOPY tp_endereco, 
 		cep VARCHAR2,
@@ -23,15 +23,15 @@ CREATE OR REPLACE TYPE BODY tp_endereco AS
 
 		END;
 END;
-
+/
 -- Telefone 
 CREATE OR REPLACE TYPE tp_telefone AS OBJECT (
 	numero_telefone VARCHAR2(10)
 );
-
+/
 -- VARRAY DE TELEFONE
 CREATE OR REPLACE TYPE tp_arr_fones AS VARRAY(5) OF tp_telefone;
-
+/
 
 -- Pessoa 
 CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
@@ -43,7 +43,7 @@ CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
 	MEMBER FUNCTION nomePessoa(P tp_pessoa) RETURN VARCHAR2,
     MEMBER PROCEDURE detalhesPessoa (P tp_pessoa)
 ) NOT FINAL NOT INSTANTIABLE;
-
+/
 --Retorna Nome da pessoa
 CREATE OR REPLACE TYPE BODY tp_pessoa AS
 	MEMBER FUNCTION nomePessoa(P tp_pessoa) RETURN VARCHAR2 IS
@@ -51,7 +51,7 @@ CREATE OR REPLACE TYPE BODY tp_pessoa AS
 		RETURN P.nome_completo;
 	END;
 END;
-
+/
 --retorna os detalhes da pessoa
 CREATE OR REPLACE TYPE BODY tp_pessoa AS
 	MEMBER PROCEDURE exibirDetalhesPedido (P tp_pessoa) IS
@@ -62,7 +62,7 @@ CREATE OR REPLACE TYPE BODY tp_pessoa AS
 		DBMS_OUTPUT.PUT_LINE('Data de NAscimento: '||to_char(P.data_nascimento));
 	END;
 END;
-
+/
 
 -- Cargo
 CREATE OR REPLACE TYPE tp_cargo AS OBJECT (
@@ -71,7 +71,7 @@ CREATE OR REPLACE TYPE tp_cargo AS OBJECT (
 	FINAL MEMBER FUNCTION salarioAnual RETURN NUMBER,
     	ORDER MEMBER FUNCTION comparaSalario (X tp_cargo) RETURN INTEGER
 );
-
+/
 --Salario anual gerando *12
 CREATE OR REPLACE TYPE BODY tp_cargo IS 
 	MEMBER FUNCTION salarioAnual RETURN NUMBER IS 
@@ -79,6 +79,7 @@ CREATE OR REPLACE TYPE BODY tp_cargo IS
 		RETURN salario *12;
 	END;
 END;
+/
 --Comparando Salarios
 CREATE OR REPLACE TYPE BODY comparaSalario AS 
 ORDER MEMBER FUNCTION comparaSalario(X tp_cargo) RETURN INTEGER IS
@@ -86,20 +87,23 @@ ORDER MEMBER FUNCTION comparaSalario(X tp_cargo) RETURN INTEGER IS
         RETURN X.salario - X.salario;
     END;
 END;
-
+/
 -- Funcionario (herda de pessoa)
 CREATE OR REPLACE TYPE tp_funcionario UNDER tp_pessoa (
 	cargo REF tp_cargo,
-	OVERRIDING MEMBER FUNCTION nomePessoa RETURN VARCHAR2 AS nomeFuncionario
+	OVERRIDING MEMBER FUNCTION nomePessoa(P tp_pessoa) RETURN VARCHAR2
 ) NOT FINAL NOT INSTANTIABLE;
-
+/
 --Overriding Member Function de Pessoa
-OVERRIDING MEMBER FUNCTION nomePessoa(f1 tp_funcionario) RETURN VARCHAR2 AS nomeFuncionario IS
-	BEGIN
-		RETURN f1.nome;
-	END;
+CREATE OR REPLACE TYPE BODY tp_funcionario 
+IS 
+OVERRIDING MEMBER FUNCTION nomePessoa(f tp_funcionario) RETURN VARCHAR2 AS nomeFuncionario 
+IS
+    BEGIN
+        RETURN f.nome;
+    END;
 END;
-
+/
 -- Dependente 
 CREATE OR REPLACE TYPE tp_dependente AS OBJECT (
 	cpf_funcionario REF tp_funcionario,
@@ -107,15 +111,17 @@ CREATE OR REPLACE TYPE tp_dependente AS OBJECT (
 	grau_parentesco VARCHAR(10),
 	data_nasc DATE
 );
-
+/
 -- NESTED TABLES:
 -- Dependentes de Biólogos 
 CREATE OR REPLACE TYPE tp_nt_dependentes_biologos AS TABLE OF tp_dependente;
+/
 -- Dependentes de Atendentes
 CREATE OR REPLACE TYPE tp_nt_dependentes_atendentes AS TABLE OF tp_dependente;
+/
 -- Dependentes de Zeladores
 CREATE OR REPLACE TYPE tp_nt_dependentes_zeladores AS TABLE OF tp_dependente;
-
+/
 
 -- Biologo
 -- herda de funcionario 
@@ -124,7 +130,7 @@ CREATE OR REPLACE TYPE tp_biologo UNDER tp_funcionario (
 	cpf_supervisor REF tp_biologo,
 	dependentes_de_biologos tp_nt_dependentes_biologos
 );
-
+/
 -- Atendente
 -- herda de funcionario
 CREATE OR REPLACE TYPE tp_atendente UNDER tp_funcionario (
@@ -132,7 +138,7 @@ CREATE OR REPLACE TYPE tp_atendente UNDER tp_funcionario (
 	cpf_supervisor REF tp_atendente,
 	dependentes_de_atendentes tp_nt_dependentes_atendentes
 ); 
-
+/
 -- Zelador
 -- herda de funcionario 
 CREATE OR REPLACE TYPE tp_zelador UNDER tp_funcionario (
@@ -140,46 +146,46 @@ CREATE OR REPLACE TYPE tp_zelador UNDER tp_funcionario (
 	cpf_supervisor REF tp_zelador,
 	dependentes_de_zeladores tp_nt_dependentes_zeladores
 );
-
+/
 -- Visitante 
 -- herda de pessoa
 CREATE OR REPLACE TYPE tp_visitante UNDER tp_pessoa (
 	data_cadastro DATE,
 	carteira_de_estudante VARCHAR(12)
 );
-
+/
 -- Jaula 
 CREATE OR REPLACE TYPE tp_jaula AS OBJECT (
 	cod_jaula VARCHAR2(5),
 	capacidade NUMBER
 );
-
+/
 -- Departamento
 CREATE OR REPLACE TYPE tp_departamento AS OBJECT (
 	cod_departamento VARCHAR2(5),
 	nome_departamento VARCHAR2(20),
 	quantidade_de_jaulas NUMBER
 );
-
+/
 -- Animais 
 CREATE OR REPLACE TYPE tp_animais AS OBJECT (
 	cod_animal VARCHAR2(5),
 	especie VARCHAR2(20)
 );
-
+/
 -- Cuida
 CREATE OR REPLACE TYPE tp_cuida AS OBJECT (
 	cod_animal REF tp_animais,
 	cpf_biologo REF tp_biologo
 );
-
+/
 -- Limpa 
 CREATE OR REPLACE TYPE tp_limpa AS OBJECT (
 	cpf_zelador REF tp_zelador,
 	cod_jaula REF tp_jaula,
 	data_limpeza DATE
 );
-
+/
 -- Ticket 
 CREATE OR REPLACE TYPE tp_ticket AS OBJECT (
 	numero_ticket NUMBER,
@@ -187,7 +193,7 @@ CREATE OR REPLACE TYPE tp_ticket AS OBJECT (
 	valor NUMBER,
 	data_ticket DATE
 );
-
+/
 -- Compra 
 CREATE OR REPLACE TYPE tp_compra AS OBJECT (
 	numero_ticket REF tp_ticket,
@@ -195,7 +201,7 @@ CREATE OR REPLACE TYPE tp_compra AS OBJECT (
 	nota_fiscal INTEGER,
 	MEMBER PROCEDURE print_compra
 );
-
+/
 -- OVERRIDING MEMBER PROCEDURE que imprime as informções da compra
 CREATE OR REPLACE TYPE BODY tp_compra AS 
 MEMBER PROCEDURE print_compra IS 
@@ -205,7 +211,7 @@ MEMBER PROCEDURE print_compra IS
 		dbms_out.put_line(nota_fiscal)
 	END;
 END;
-
+/
 -- Promocao 
 CREATE OR REPLACE TYPE tp_promocao AS OBJECT (
 	cod_promocao NUMBER,
@@ -214,7 +220,7 @@ CREATE OR REPLACE TYPE tp_promocao AS OBJECT (
 	data_termino DATE,
 	MAP MEMBER FUNCTION promocaoTOnumber RETURN NUMBER
 );
-
+/
 CREATE OR REPLACE TYPE BODY tp_promocao AS
 MAP MEMBER FUNCTION promocaoTOnumber RETURN INTEGER IS
 p NUMBER := codigo_promocao;
@@ -222,7 +228,7 @@ p NUMBER := codigo_promocao;
 		RETURN p;
 	END;
 END;
-
+/
 
 -- Participa 
 CREATE OR REPLACE TYPE tp_participa AS OBJECT (
@@ -230,7 +236,7 @@ CREATE OR REPLACE TYPE tp_participa AS OBJECT (
 	numero_ticket REF tp_ticket,
 	cpf_visitante REF tp_visitante
 );
-
+/
 -- Pertence 
 CREATE OR REPLACE TYPE tp_pertence AS OBJECT (
 	animais REF tp_animais,
@@ -239,5 +245,6 @@ CREATE OR REPLACE TYPE tp_pertence AS OBJECT (
 	departamento REF tp_departamento,
 	data_saida DATE
 );
+/
 --Modificando o Funcionario
 ALTER TYPE tp_funcionario ADD ATTRIBUTE data_de_contratacao DATE CASCADE;
