e3ecaf94efeca7df94c4b005241e24ba1626de86
DanielVCC <43818658+DanielVCC@users.noreply.github.com>
2022-10-09 19:55:25 -0300

    Ajustes Criação dos tipos
    Atualizações e mudanças em algumas funções dos tipos criados

--- OR/CriacaoDosTipos.sql
+++ OR/CriacaoDosTipos.sql
@@ -4,7 +4,12 @@ CREATE OR REPLACE TYPE tp_endereco AS OBJECT (
 	cep VARCHAR2(9),
 	rua VARCHAR2(30),
 	numero NUMBER,
-	bairro VARCHAR2(20)
+	bairro VARCHAR2(20),
+	CONSTRUCTOR FUNCTION tp_endereco(SELF IN OUT NOCOPY tp_endereco, 
+		cep VARCHAR2,
+		rua VARCHAR2,
+		numero NUMBER,
+		bairro VARCHAR2) RETURN SELF AS RESULT
 );
 /
 CREATE OR REPLACE TYPE BODY tp_endereco AS
@@ -40,27 +45,26 @@ CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
 	data_nascimento DATE,
 	endereco tp_endereco,
 	telefones tp_arr_fones,
-	MEMBER FUNCTION nomePessoa(P tp_pessoa) RETURN VARCHAR2,
-    MEMBER PROCEDURE detalhesPessoa (P tp_pessoa)
+	FINAL MEMBER FUNCTION numeroTelefones RETURN NUMBER,
+    MEMBER PROCEDURE detalhesPessoa (SELF tp_pessoa)
 ) NOT FINAL NOT INSTANTIABLE;
 /
---Retorna Nome da pessoa
+
 CREATE OR REPLACE TYPE BODY tp_pessoa AS
-	MEMBER FUNCTION nomePessoa(P tp_pessoa) RETURN VARCHAR2 IS
+	-- retorna a quantidade de numeros registrados da pessoa
+	FINAL MEMBER FUNCTION numeroTelefones RETURN NUMBER IS
 	BEGIN
-		RETURN P.nome_completo;
+		RETURN telefones.COUNT;
 	END;
-END;
-/
---retorna os detalhes da pessoa
-CREATE OR REPLACE TYPE BODY tp_pessoa AS
-	MEMBER PROCEDURE exibirDetalhesPedido (P tp_pessoa) IS
+	   -- printa os detalhes da pessoa
+	MEMBER PROCEDURE detalhesPessoa (SELF tp_pessoa) IS
 	BEGIN
 		DBMS_OUTPUT.PUT_LINE('Detalhes da Pessoa:');
-		DBMS_OUTPUT.PUT_LINE('CPF:'||P.cpf);
-		DBMS_OUTPUT.PUT_LINE('Nome: '||P.nome));
-		DBMS_OUTPUT.PUT_LINE('Data de NAscimento: '||to_char(P.data_nascimento));
+		DBMS_OUTPUT.PUT_LINE('CPF:'|| cpf);
+		DBMS_OUTPUT.PUT_LINE('Nome: '|| nome);
+		DBMS_OUTPUT.PUT_LINE('Data de Nascimento: '||to_char(data_nascimento));
 	END;
+
 END;
 /
 
@@ -68,49 +72,60 @@ END;
 CREATE OR REPLACE TYPE tp_cargo AS OBJECT (
 	cargo_funcionario VARCHAR2 (20),
 	salario NUMBER,
-	FINAL MEMBER FUNCTION salarioAnual RETURN NUMBER,
-    	ORDER MEMBER FUNCTION comparaSalario (X tp_cargo) RETURN INTEGER
+	MAP MEMBER FUNCTION salarioAnual RETURN NUMBER
 );
 /
 --Salario anual gerando *12
-CREATE OR REPLACE TYPE BODY tp_cargo IS 
-	MEMBER FUNCTION salarioAnual RETURN NUMBER IS 
+CREATE OR REPLACE TYPE BODY tp_cargo AS 
+	MAP MEMBER FUNCTION salarioAnual RETURN NUMBER IS 
 	BEGIN
 		RETURN salario *12;
 	END;
 END;
 /
---Comparando Salarios
-CREATE OR REPLACE TYPE BODY comparaSalario AS 
-ORDER MEMBER FUNCTION comparaSalario(X tp_cargo) RETURN INTEGER IS
-    BEGIN 
-        RETURN X.salario - X.salario;
-    END;
-END;
-/
+
 -- Funcionario (herda de pessoa)
 CREATE OR REPLACE TYPE tp_funcionario UNDER tp_pessoa (
 	cargo REF tp_cargo,
-	OVERRIDING MEMBER FUNCTION nomePessoa(P tp_pessoa) RETURN VARCHAR2
+	OVERRIDING MEMBER PROCEDURE detalhesPessoa (SELF tp_funcionario)
 ) NOT FINAL NOT INSTANTIABLE;
 /
---Overriding Member Function de Pessoa
-CREATE OR REPLACE TYPE BODY tp_funcionario 
-IS 
-OVERRIDING MEMBER FUNCTION nomePessoa(f tp_funcionario) RETURN VARCHAR2 AS nomeFuncionario 
-IS
-    BEGIN
-        RETURN f.nome;
-    END;
+--Overriding Member procedure de Pessoa
+CREATE OR REPLACE TYPE BODY tp_funcionario AS 
+	OVERRIDING MEMBER PROCEDURE detalhesPessoa (SELF tp_funcionario) IS
+	BEGIN
+		DBMS_OUTPUT.PUT_LINE('Detalhes do Funcionario:');
+		DBMS_OUTPUT.PUT_LINE('CPF:'|| cpf);
+		DBMS_OUTPUT.PUT_LINE('Nome: '|| nome);
+		DBMS_OUTPUT.PUT_LINE('Data de Nascimento: '||to_char(data_nascimento));
+	END;
 END;
 /
 -- Dependente 
 CREATE OR REPLACE TYPE tp_dependente AS OBJECT (
 	nome_dependente VARCHAR2(20),
 	grau_parentesco VARCHAR(10),
-	data_nasc DATE
+	data_nasc DATE,
+	ORDER MEMBER FUNCTION comparaDependente(D tp_dependente) RETURN INTEGER
 );
 /
+CREATE OR REPLACE TYPE BODY tp_dependente AS 
+	ORDER MEMBER FUNCTION comparaDependente(D tp_dependente) RETURN INTEGER IS
+	selfid VARCHAR2(250) := SELF.nome_dependente || SELF.grau_parentesco || to_char(SELF.data_nasc);
+	otherid VARCHAR2(250) := D.nome_dependente || D.grau_parentesco || to_char(D.data_nasc);
+	BEGIN
+		IF(selfid > otherid) THEN
+			RETURN 1;
+		END IF;
+		IF (selfid = otherid) THEN
+			RETURN 0;
+		END IF;
+		RETURN -1;
+	END;
+END;
+/
+
+
 -- NESTED TABLES:
 -- Dependentes de Biólogos 
 CREATE OR REPLACE TYPE tp_nt_dependentes_biologos AS TABLE OF tp_dependente;
@@ -197,34 +212,23 @@ CREATE OR REPLACE TYPE tp_ticket AS OBJECT (
 CREATE OR REPLACE TYPE tp_compra AS OBJECT (
 	numero_ticket REF tp_ticket,
 	cpf REF tp_visitante,
-	nota_fiscal INTEGER,
-	MEMBER PROCEDURE print_compra
+	nota_fiscal INTEGER
 );
 /
--- OVERRIDING MEMBER PROCEDURE que imprime as informções da compra
-CREATE OR REPLACE TYPE BODY tp_compra AS 
-MEMBER PROCEDURE print_compra IS 
-	BEGIN 
-		dbms_out.put_line(numero_ticket);
-		dbms_out.put_line(cpf_visitante);
-		dbms_out.put_line(nota_fiscal);
-	END;
-END;
-/
+
 -- Promocao 
 CREATE OR REPLACE TYPE tp_promocao AS OBJECT (
 	cod_promocao NUMBER,
 	desconto NUMBER,
 	data_inicio DATE,
 	data_termino DATE,
-	MAP MEMBER FUNCTION promocaoTOnumber RETURN NUMBER
+	MEMBER FUNCTION promocaoInfo RETURN VARCHAR
 );
 /
 CREATE OR REPLACE TYPE BODY tp_promocao AS
-MAP MEMBER FUNCTION promocaoTOnumber RETURN INTEGER IS
-p NUMBER := codigo_promocao;
+MEMBER FUNCTION promocaoInfo RETURN VARCHAR IS
 	BEGIN
-		RETURN p;
+		RETURN ('O código ' || cod_promocao || ' é válido até ' || to_char(data_termino));
 	END;
 END;
 /
@@ -245,5 +249,5 @@ CREATE OR REPLACE TYPE tp_pertence AS OBJECT (
 	data_saida DATE
 );
 /
-	--Modificando o Funcionario
- -- ALTER TYPE tp_funcionario ADD ATTRIBUTE data_de_contratacao DATE CASCADE;
+--Modificando o Funcionario
+--+ALTER TYPE tp_funcionario ADD ATTRIBUTE data_de_contratacao DATE CASCADE;
