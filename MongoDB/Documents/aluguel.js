74ca7ba1adefc92f9eadbb9dc81da930e168bc5b
Gustavo Borges <ggb@cin.ufpe.br>
2022-10-22 20:16:38 -0300

    Inserção na coleção 'aluguel'

---
+++ MongoDB/Documents/aluguel.js
@@ -0,0 +1,18 @@
+db.aluguel.insertMany([
+    {
+        _id: 1,
+        id_cliente: 9,
+        id_produto: 10,
+        data_de_aluguel: '22-10-2022',
+        data_de_devolucao: '25-10-2022',
+        entregue: false
+    },
+    {
+        _id: 2,
+        id_cliente: 8,
+        id_produto: 5,
+        data_de_aluguel: '20-10-2022',
+        data_de_devolucao: '22-10-2022',
+        entregue: false
+    }
+])
