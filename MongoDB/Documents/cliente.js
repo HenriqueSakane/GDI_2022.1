77485472e04145e59f3327a8d626a37f5fc03caa
Daniel Victor Cintra Cavalcante <43818658+DanielVCC@users.noreply.github.com>
2022-10-24 18:09:01 -0300

    Update cliente.js

--- MongoDB/Documents/cliente.js
+++ MongoDB/Documents/cliente.js
@@ -1,30 +1,30 @@
 db.cliente.insertMany([
   { 
-    _id: 1,
+    _id: ObjectId("6356fc576bea5973dc6a030a"),
     pessoa: 8,
     vip: true,
     dataRegistro: new Date('2022-09-25T08:00:00Z') 
   },
   { 
-    _id: 2, 
+    _id: ObjectId("6356fc903a7d479925ec3c54"), 
     pessoa: 6, 
     vip: true, 
     dataRegistro: new Date('2022-09-30T09:00:00Z') 
   },
   { 
-    _id: 3, 
+    _id: ObjectId("6356fc9a9d1b06b500611c94"), 
     pessoa: 5, 
     vip: false, 
     dataRegistro: new Date('2022-10-01T09:00:00Z') 
   },
   { 
-    _id: 4, 
+    _id: ObjectId("6356fca773f81b21dfc5fc19"), 
     pessoa: 7, 
     vip: false, 
     dataRegistro: new Date('2022-10-01T11:21:39.736Z') 
   },
   { 
-    _id: 5, 
+    _id: ObjectId("6356fcb26483e0787993fc5b"), 
     pessoa: 10, 
     vip: true, 
     dataRegistro: new Date('2022-10-04T21:23:13.331Z') 
