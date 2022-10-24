c353cb46e1734e1292ad44a6967a4220e1ad74b1
IgorDomingos <idrs@cin.ufpe.br>
2022-10-24 14:59:02 -0300

    Adicionando funcionarios

---
+++ MongoDB/Documents/funcionario.js
@@ -0,0 +1,38 @@
+db.funcionario.insertMany([
+  { 
+    _id: 1,
+    pessoa: 2,
+    cargo: "Gerente",
+    dataContratacao: "02/09/2022",
+    salario: 2500,
+  },
+  { 
+    _id: 2, 
+    pessoa: 1, 
+    cargo: "Vendedor",
+    dataContratacao: "05/09/2022",
+    salario: 2000,
+  },
+  { 
+    _id: 3, 
+    pessoa: 3, 
+    cargo: "Vendedor",
+    dataContratacao: "20/09/2022",
+    salario: 2000, 
+  },
+  { 
+    _id: 4, 
+    pessoa: 4, 
+    cargo: "Vendedor",
+    dataContratacao: "10/10/2022",
+    salario: 2000, 
+  },
+  { 
+    _id: 5, 
+    pessoa: 9, 
+    cargo: "Zelador",
+    dataContratacao: "15/09/2022",
+    salario: 1500, 
+  },
+]);
+
