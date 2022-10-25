406654c5d4312bd0d7cc2a974d6df7a1a0666203
Gustavo Borges <ggb@cin.ufpe.br>
2022-10-25 03:26:55 -0300

    Inserção de novos alugueis e adicionados os comandos 'remove' e 'update' para aluguel

--- MongoDB/Documents/aluguel.js
+++ MongoDB/Documents/aluguel.js
@@ -1,3 +1,5 @@
+// Inserção de documentos
+
 db.aluguel.insertMany([
     {
         _id: 1,
@@ -13,7 +15,7 @@ db.aluguel.insertMany([
         produtos_id: [5],
         data_de_aluguel: '20-10-2022',
         data_de_devolucao: '22-10-2022',
-        devolvido: false
+        devolvido: true
     },
     {
         _id: 3,
@@ -32,20 +34,72 @@ db.aluguel.insertMany([
         devolvido: true
     },
     {
-        _id: 5,
+        _id: 6,
         id_cliente: ObjectId("6356fcb26483e0787993fc5b"), // Pessoa 10
         produtos_id: [1],
         data_de_aluguel: '20-10-2022',
         data_de_devolucao: '25-10-2022',
         devolvido: false
     },
+    // Alugueis a serem atualizados
     {
-        _id: 5,
+        _id: 7,
         id_cliente: ObjectId("6356fca773f81b21dfc5fc19"), // Pessoa 7
         produtos_id: [6],
+        data_de_aluguel: '24-10-2022',
+        data_de_devolucao: '26-10-2022',
+        devolvido: false
+    },
+    {
+        _id: 8,
+        id_cliente: ObjectId("6356fc9a9d1b06b500611c94"), // Pessoa 5
+        produtos_id: [6],
+        data_de_aluguel: '21-10-2022',
+        data_de_devolucao: '22-10-2022',
+        devolvido: true
+    },
+    {
+        _id: 9,
+        id_cliente: ObjectId("6356fc576bea5973dc6a030a"), // Pessoa 8
+        produtos_id: [2, 3],
+        data_de_aluguel: '25-10-2022',
+        data_de_devolucao: '27-10-2022',
+        devolvido: false
+    },
+    // Aluguel a ser removido
+    {
+        _id: 10,
+        id_cliente: ObjectId("6356fca773f81b21dfc5fc19"), // Pessoa 7
+        produtos_id: [1],
         data_de_aluguel: '22-10-2022',
         data_de_devolucao: '26-10-2022',
         devolvido: false
     }
 
 ])
+
+// Remoção de documento
+
+db.aluguel.remove({ _id: 10  })
+
+// Atualização de documento
+
+db.aluguel.update(
+    {   produtos: [6]   },
+    {
+        $set: { produtos: [6, 7] }    
+    },
+    {   multi: true     }
+)
+
+db.aluguel.update( {data_de_devolucao: '27-10-2022'}, {$set: {data_de_devolucao: '28-10-2022'}} )
+
+db.aluguel.update(
+    {   data_de_aluguel: '20-10-2022'   },
+    {
+        $set: {
+            data_de_aluguel: '21-10-2022',
+            data_de_devolucao: '26-10-2022'
+        }
+    }
+)
