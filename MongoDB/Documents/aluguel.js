4534a73e644b34d670a6a87a61add89064a70595
Gustavo Borges <ggb@cin.ufpe.br>
2022-10-26 20:56:19 -0300

    ajuste em aluguel ids filmes

--- MongoDB/Documents/aluguel.js
+++ MongoDB/Documents/aluguel.js
@@ -15,11 +15,11 @@ db.aluguel.insertMany([
         _id: 3,
         id_cliente: ObjectId("6356fc903a7d479925ec3c54"), // Pessoa 6
         jogos_id: [5],
-        filmes_id: [],
+        filmes_id: [1, 10],
         data_de_aluguel: new Date("2022-10-20"),
         data_de_devolucao: new Date("2022-10-22"),
         devolvido: true,
-        preco: 18.00
+        preco: 47.00
     },
     {
         _id: 2,
@@ -45,17 +45,17 @@ db.aluguel.insertMany([
         _id: 4,
         id_cliente: ObjectId("6356fcb26483e0787993fc5b"), // Pessoa 10
         jogos_id: [1],
-        filmes_id: [],
+        filmes_id: [5],
         data_de_aluguel: new Date("2022-10-20"),
         data_de_devolucao: new Date("2022-10-25"),
         devolvido: false,
-        preco: 15.00
+        preco: 25.00
     },
     {
         _id: 7,
         id_cliente: ObjectId("6356fc903a7d479925ec3c54"), // Pessoa 6
-        jogos_id: [6, 7],
-        filmes_id: [],
+        jogos_id: [],
+        filmes_id: [7, 4],
         data_de_aluguel: new Date("2022-10-23"),
         data_de_devolucao: new Date("2022-10-26"),
         devolvido: false,
@@ -76,17 +76,17 @@ db.aluguel.insertMany([
         _id: 5,
         id_cliente: ObjectId("6356fc9a9d1b06b500611c94"), // Pessoa 5
         jogos_id: [6],
-        filmes_id: [],
+        filmes_id: [3, 8],
         data_de_aluguel: new Date("2022-10-21"),
         data_de_devolucao: new Date("2022-10-22"),
         devolvido: true,
-        preco: 17.00
+        preco: 47.00
     },
     {
         _id: 9,
         id_cliente: ObjectId("6356fc576bea5973dc6a030a"), // Pessoa 8
         jogos_id: [2, 3],
-        filmes_id: [],
+        filmes_id: [10],
         data_de_aluguel: new Date("2022-10-25"),
         data_de_devolucao: new Date("2022-10-27"),
         devolvido: false,
@@ -97,11 +97,11 @@ db.aluguel.insertMany([
         _id: 10,
         id_cliente: ObjectId("6356fca773f81b21dfc5fc19"), // Pessoa 7
         jogos_id: [1],
-        filmes_id: [],
+        filmes_id: [3],
         data_de_aluguel: new Date("2022-10-22"),
         data_de_devolucao: new Date("2022-10-26"),
         devolvido: false,
-        preco: 15.00
+        preco: 25.00
     }
 
 ])
