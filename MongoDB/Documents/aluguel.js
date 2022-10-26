798825c0235dbb407139ff6954bac2f11542f64b
Gustavo Borges <ggb@cin.ufpe.br>
2022-10-26 20:53:45 -0300

    ajuste em aluguel

--- MongoDB/Documents/aluguel.js
+++ MongoDB/Documents/aluguel.js
@@ -4,7 +4,8 @@ db.aluguel.insertMany([
     {
         _id: 6,
         id_cliente: ObjectId("6356fc576bea5973dc6a030a"), // Pessoa 8
-        produtos_id: [10],
+        jogos_id: [10],
+        filmes_id: [],
         data_de_aluguel: new Date("2022-10-22"),
         data_de_devolucao: new Date("2022-10-25"),
         devolvido: false,
@@ -13,7 +14,8 @@ db.aluguel.insertMany([
     {
         _id: 3,
         id_cliente: ObjectId("6356fc903a7d479925ec3c54"), // Pessoa 6
-        produtos_id: [5],
+        jogos_id: [5],
+        filmes_id: [],
         data_de_aluguel: new Date("2022-10-20"),
         data_de_devolucao: new Date("2022-10-22"),
         devolvido: true,
@@ -22,7 +24,8 @@ db.aluguel.insertMany([
     {
         _id: 2,
         id_cliente: ObjectId("6356fc9a9d1b06b500611c94"), // Pessoa 5
-        produtos_id: [8, 4],
+        jogos_id: [8, 4],
+        filmes_id: [],
         data_de_aluguel: new Date("2022-10-18"),
         data_de_devolucao: new Date("2022-10-22"),
         devolvido: false,
@@ -31,7 +34,8 @@ db.aluguel.insertMany([
     {
         _id: 1,
         id_cliente: ObjectId("6356fca773f81b21dfc5fc19"), // Pessoa 7
-        produtos_id: [3, 7, 2],
+        jogos_id: [3, 7, 2],
+        filmes_id: [],
         data_de_aluguel: new Date("2022-10-15"),
         data_de_devolucao: new Date("2022-10-18"),
         devolvido: true,
@@ -40,7 +44,8 @@ db.aluguel.insertMany([
     {
         _id: 4,
         id_cliente: ObjectId("6356fcb26483e0787993fc5b"), // Pessoa 10
-        produtos_id: [1],
+        jogos_id: [1],
+        filmes_id: [],
         data_de_aluguel: new Date("2022-10-20"),
         data_de_devolucao: new Date("2022-10-25"),
         devolvido: false,
@@ -49,7 +54,8 @@ db.aluguel.insertMany([
     {
         _id: 7,
         id_cliente: ObjectId("6356fc903a7d479925ec3c54"), // Pessoa 6
-        produtos_id: [6, 7],
+        jogos_id: [6, 7],
+        filmes_id: [],
         data_de_aluguel: new Date("2022-10-23"),
         data_de_devolucao: new Date("2022-10-26"),
         devolvido: false,
@@ -59,7 +65,8 @@ db.aluguel.insertMany([
     {
         _id: 8,
         id_cliente: ObjectId("6356fca773f81b21dfc5fc19"), // Pessoa 7
-        produtos_id: [6],
+        jogos_id: [6],
+        filmes_id: [],
         data_de_aluguel: new Date("2022-10-24"),
         data_de_devolucao: new Date("2022-10-26"),
         devolvido: false,
@@ -68,7 +75,8 @@ db.aluguel.insertMany([
     {
         _id: 5,
         id_cliente: ObjectId("6356fc9a9d1b06b500611c94"), // Pessoa 5
-        produtos_id: [6],
+        jogos_id: [6],
+        filmes_id: [],
         data_de_aluguel: new Date("2022-10-21"),
         data_de_devolucao: new Date("2022-10-22"),
         devolvido: true,
@@ -77,7 +85,8 @@ db.aluguel.insertMany([
     {
         _id: 9,
         id_cliente: ObjectId("6356fc576bea5973dc6a030a"), // Pessoa 8
-        produtos_id: [2, 3],
+        jogos_id: [2, 3],
+        filmes_id: [],
         data_de_aluguel: new Date("2022-10-25"),
         data_de_devolucao: new Date("2022-10-27"),
         devolvido: false,
@@ -87,7 +96,8 @@ db.aluguel.insertMany([
     {
         _id: 10,
         id_cliente: ObjectId("6356fca773f81b21dfc5fc19"), // Pessoa 7
-        produtos_id: [1],
+        jogos_id: [1],
+        filmes_id: [],
         data_de_aluguel: new Date("2022-10-22"),
         data_de_devolucao: new Date("2022-10-26"),
         devolvido: false,
