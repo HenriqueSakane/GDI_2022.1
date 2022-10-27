7b19eb6daa083b44bc842c775eb9139021d1b0f6
Gustavo Borges <ggb@cin.ufpe.br>
2022-10-26 22:37:07 -0300

    $size

--- MongoDB/Queries/queries.js
+++ MongoDB/Queries/queries.js
@@ -80,12 +80,11 @@ db.pessoa.findOne({ cpf: "706.124.236-92" }, { _id: 0, nome: 1, cpf: 1, email: 1
 // Pegar os três funcionarios de maior salario
 db.funcionario.find().sort({ valor: -1 }).limit(3);
 
-// Alugueis com 2 ou 3 produtos, agrupados pela situação de entrega (devolvido ou não) e calculando seus preços medios 
+// Alugueis com 3 jogos, agrupados pela situação de entrega (devolvido ou não) e calculando seus preços medios 
 db.aluguel.aggregate([
     {
-        $match: {   $or: [ {id_produtos: {  $size: 2  }}, {id_produtos: {  $size: 3  }} ]   }
+        jogos_id: { $size: 3 }
     },
-
     {
         $group: {   
             _id: "$devolvido",
